a1ea668c6dc41fa5c54232eb93369f2e27bce61f
Matheus Magalhaes <magal97@outlook.com>
2020-06-18 00:42:00 -0300

    "App_URL"

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(routes);
 app.use(errors());
 
-app.listen(process.env.PORT || 3333, () => {
+app.listen(process.env.APP_URL || 3333, () => {
     console.log('🚀 Server started on port 3333!');
-  });
+});
   
